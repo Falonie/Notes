56e968b927ba5e6326bcf3a936738baa65578767
Wangqingchen <541002901@qq.com>
2017-07-07 19:05:05 +0800

    update

--- json1.py
+++ json1.py
@@ -1,3 +1,55 @@
-import json
+import json,random
+
 d=dict(name='caren',salary=18000)
-print(json.dumps(d))
+print(json.dumps(d),type(d))
+print('\n')
+
+json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
+parsed_json=json.loads(json_string)
+print(parsed_json['first_name'],type(parsed_json))
+
+d = {
+    'first_name': 'Guido',
+    'second_name': 'Rossum',
+    'titles': ['BDFL', 'Developer'],
+}
+f=json.dumps(d)
+print(f,type(f))
+print(d,type(d))
+print('\n')
+
+def main():
+    p = []
+    with open('ip2.txt', 'r') as f:
+        # print(f.readlines()[0])
+        # ipaddress = f.readlines()[0]
+        # line = [i for i in f.readlines()]
+        for i in f.readlines():
+            lines = json.loads(i)
+            # print(lines)
+            # print(type(lines))
+            for line in lines:
+                print(line, type(line), line['ip'])
+                p.append(line['ip'])
+            print(p)
+            return p
+
+if __name__=='__main__':
+    main()
+    # print(random.choice(main()),type(random.choice(main())))
+    print(random.choice(main()))
+
+def readip():
+    pool = []
+    with open('ip.txt','r') as f:
+        for i in f.readlines():
+            lines = json.loads(i)
+            line = lines['RESULT']
+            for l in line:
+                print(l, l.get('ip'), type(l))
+                pool.append(l.get('ip'))
+            return pool
+
+if __name__=='__main__':
+    readip()
+    print(random.choice(readip()))
---
+++ json2.py
@@ -0,0 +1,38 @@
+import json
+
+d=dict(name='Caseny',salary=18000)
+a=json.dumps(d)
+print(a,type(d),type(a))
+
+f='{"name":"kitty","age":"20"}'
+# f='{"first_name": "Guido", "last_name":"Rossum"}'
+b=json.loads(f)
+print(b['name'],type(b),b,type(f))
+print('\n')
+
+class Student(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+    def __repr__(self):
+        return 'name:{} age:{} score:{}'.format(self.name,self.age,self.score)
+
+def studentdict(std):
+    return{
+        'name':std.name,
+        'age':std.age,
+        'score':std.score
+    }
+
+s=Student('kitty',20,90)
+print(s,type(s))
+s1=json.dumps(s,default=studentdict)
+print(s1,type(s1))
+s2=json.dumps(s,default=lambda x:x.__dict__)
+print(s2,type(s2))
+
+print('\n')
+s3=json.loads(s2)
+print(s3,type(s3))
